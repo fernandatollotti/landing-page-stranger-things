34b85f46e1a77e135270454f4effa15692affbfa
Fernanda Tollotti <fernandatollotti@gmail.com>
2024-12-07 17:32:13 -0300

    feat: implement play button functionality for video

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,15 @@
+const playButton = document.querySelector('.thumbnail .play');
+
+playButton.addEventListener('click', () => {
+  const videoFrame = document.querySelector('.video-frame');
+  const iframe = document.querySelector('iframe');
+
+  videoFrame.classList.add('playing');
+
+  iframe.src += '&autoplay=1';
+  console.log(iframe.src);
+})
+
 function switchTheme() {
   const body = document.body;
   const dataTheme = body.getAttribute('data-theme');
